88dafce3de351b554ceb0f1f6604ff499f8be2e3
Caracol4M <30309627+SM0K1NG@users.noreply.github.com>
2023-11-23 20:21:39 -0500

    Update findstr.sh

--- findstr.sh
+++ findstr.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Verifica se o número de argumentos é correto
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <caminho_do_diretorio> <string_a_procurar>"
+    exit 1
+fi
+
+# Atribui os argumentos a variáveis
+diretorio_inicial="$1"
+string_a_procurar="$2"
+
 # Função para percorrer recursivamente os diretórios
 search_string() {
     for file in "$1"/*; do
@@ -8,15 +18,22 @@ search_string() {
             search_string "$file"
         elif [ -f "$file" ]; then
             # Se for um arquivo, verifica se a string está presente
-            if grep -q "tray" "$file"; then
+            echo "Verificando arquivo: $file"
+            if grep -q "$string_a_procurar" "$file"; then
                 echo "String encontrada em: $file"
+                echo "$file" >> resultados.txt
             fi
         fi
     done
 }
 
-# Diretório inicial (pode ser ajustado conforme necessário)
-diretorio_inicial="/var/www/html"
+# Nome do arquivo para salvar os resultados
+arquivo_resultados="resultados.txt"
+
+# Cria um novo arquivo de resultados ou limpa o existente
+> "$arquivo_resultados"
 
 # Chama a função com o diretório inicial
 search_string "$diretorio_inicial"
+
+echo "Busca concluída. Resultados salvos em: $arquivo_resultados"
